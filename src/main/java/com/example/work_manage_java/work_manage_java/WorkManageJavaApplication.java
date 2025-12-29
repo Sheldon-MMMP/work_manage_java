48fefbd069ce41d970148a32cb3ebda7b66e35a4
Ki June <sheldon-wong@qq.com>
2025-12-29 12:42:28 +0000

    chore: init project

---
+++ src/main/java/com/example/work_manage_java/work_manage_java/WorkManageJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.work_manage_java.work_manage_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkManageJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkManageJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/work_manage_java/work_manage_java/WorkManageJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.work_manage_java.work_manage_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkManageJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
